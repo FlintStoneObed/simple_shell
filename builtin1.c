0b68639d6299efc07900c360e8a869686a3ba90d
regalconcepts <naboafo18@gmail.com>
2023-12-13 21:28:50 +0000

    builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list, one command by line, preceded
+ * _myhistory - this displays the history list, one command by line, preceded
  * with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
  * constant function prototype.
@@ -15,7 +15,7 @@ int _myhistory(info_t *info)
 }
 
 /**
- * unset_alias - sets alias to string
+ * unset_alias - this sets alias to string
  * @info: parameter struct
  * @str: string alias
  *
@@ -38,7 +38,7 @@ int unset_alias(info_t *info, char *str)
 }
 
 /**
- * set_alias - sets alias to string
+ * set_alias - this sets alias to string
  * @info: parameter struct
  * @str: string alias
  *
@@ -59,7 +59,7 @@ int set_alias(info_t *info, char *str)
 }
 
 /**
- * print_alias - prints an alias string
+ * print_alias - this prints an alias string
  * @node: alias node
  *
  * Return: Always 0 on success, 1 on error
@@ -82,7 +82,7 @@ int print_alias(list_t *node)
 }
 
 /**
- * _myalias - mimics the alias builtin (man alias)
+ * _myalias - this mimics the alias builtin (man alias)
  * @info: Structure containing potential arguments. Used to maintain
  * constant function prototype.
  * 
